03f5b610e24aa8a28fa41bed0bffa98b9db83d14
ryangabriel27 <ryanjulio56@gmail.com>
2023-08-17 16:52:23 -0300

    17/08

--- PHPDosFoguetes/cadastro.php
+++ PHPDosFoguetes/cadastro.php
@@ -1,18 +1,24 @@
 <?php
-    include("conexao.php");
+    if (!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['senha']) && !empty($_POST['cpf']) ) {
+        
+        include("conexao.php");
 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $cpf = $_POST['cpf'];
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $cpf = $_POST['cpf'];
 
-    $sql = "INSERT INTO cadastro(nome,email,senha,cpf) VALUES ('$nome','$email','$senha','$cpf')";
+        $sql = "INSERT INTO cadastro(nome,email,senha,cpf) VALUES ('$nome','$email','$senha','$cpf')";
 
-    if (mysqli_query($conexao,$sql)) {
-        echo "Usuário cadastrado com sucesso!!";
+        if (mysqli_query($conexao,$sql)) {
+            echo "Usuário cadastrado com sucesso!!";
+        } else {
+            echo "Erro".mysqli_connect_error($conexao);
+        }
+
+        mysqli_close($conexao);
     } else {
-        echo "Erro".mysqli_connect_error($conexao);
+        echo "<script language='javascript' type='text/javascript'> 
+        alert('Os campos devem ser preenchidos para prosseguir'); window.location.href='cad.php';</script>";
     }
-
-    mysqli_close($conexao);
 ?>
